6570f4aaf862c89ab1c1bd38661482f7c4cf93ba
Ron Pomeroy <rpomeroy@me.com>
2014-05-04 11:43:49 -0700

    Move DashboardApplication to mbmg package

--- src/main/java/org/mbmg/DashboardApplication.java
+++ src/main/java/org/mbmg/DashboardApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.vaadin.spring.boot.sample;
+package org.mbmg;
 
 import com.vaadin.addon.charts.Chart;
 import com.vaadin.addon.charts.model.ListSeries;
