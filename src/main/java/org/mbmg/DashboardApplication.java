cf1a97360d51947264774d69a3291b03a37588df
Ron Pomeroy <rpomeroy@me.com>
2014-05-06 21:58:44 -0700

    Splash page with nice styling comes up

--- src/main/java/org/mbmg/DashboardApplication.java
+++ src/main/java/org/mbmg/DashboardApplication.java
@@ -15,31 +15,22 @@
  */
 package org.mbmg;
 
-import com.vaadin.addon.charts.Chart;
-import com.vaadin.addon.charts.model.ListSeries;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
-import com.vaadin.annotations.Widgetset;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.navigator.Navigator;
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Scope;
-import org.vaadin.spring.UIScope;
-import org.vaadin.spring.VaadinComponent;
 import org.vaadin.spring.VaadinUI;
-import org.vaadin.spring.navigator.SpringViewProvider;
-import org.vaadin.spring.navigator.VaadinView;
 
-import java.util.Date;
+import java.util.Locale;
 
 /**
  * Entry point into the Vaadin web application. You may run this from
@@ -64,99 +55,139 @@ public class DashboardApplication extends SpringBootServletInitializer {
 }
 
 @VaadinUI
-@Theme("mbmg")
-@Title("Root UI")
+@Theme("dashboard")
+@Title("Muhuru-Bay Microgrid Dashboard")
 class RootUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        CustomLayout layout = new CustomLayout("dashboard");
-        setContent(layout);
-        // Header
-        Label header = new Label("Custom layout");
-        header.addStyleName("header");
-        layout.addComponent(header, "header");
-        // Menu
-        Label menu = new Label("menu");
-        layout.addComponent(menu, "menu");
-        // Content
-        Link content = new Link("Go to other UI", new ExternalResource("dashboard"));
-        layout.addComponent(content, "content");
-        // Footer
-        Label footer = new Label(" Created by Vaadin, 2013");
-        layout.addComponent(footer, "footer");
-    }
-}
-
-@VaadinUI(path = "/dashboard")
-@Widgetset("org.mbmg.AppWidgetSet")
-@Title("Another UI")
-class AnotherUI extends UI {
-
-    @Autowired
-    SpringViewProvider viewProvider;
-    @Autowired
-    ErrorView errorView;
+    CssLayout root = new CssLayout();
+    VerticalLayout loginLayout;
+    private Navigator nav;
+    private HelpManager helpManager;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Navigator navigator = new Navigator(this, this);
-        navigator.setErrorView(errorView);
-        navigator.addProvider(viewProvider);
-        setNavigator(navigator);
-    }
-}
-
-@VaadinView(name = "")
-@UIScope
-class MyDefaultView extends VerticalLayout implements View {
-
-    @Override
-    public void enter(ViewChangeListener.ViewChangeEvent event) {
-        addComponent(new Label(
-                String.format("%s: It's %s and I was just entered!",
-                        getClass().getSimpleName(), new Date())
-        ));
-
-        Chart chart = new Chart();
-        chart.getConfiguration().addSeries(new ListSeries(1, 2, 3));
-        addComponent(chart);
-    }
-}
-
-@VaadinView(name = "myView", ui = AnotherUI.class)
-@UIScope
-class MyView extends VerticalLayout implements View {
-
-    @Override
-    public void enter(ViewChangeListener.ViewChangeEvent event) {
-        addComponent(new Label(String.format("%s: It's %s and I was just entered!", getClass().getSimpleName(), new Date())));
-    }
-}
-
-@VaadinView(name = "hello/world", ui = AnotherUI.class)
-@Scope("prototype")
-class MyViewWithCustomName extends VerticalLayout implements View {
-
-    @Override
-    public void enter(ViewChangeListener.ViewChangeEvent event) {
-        addComponent(new Label(String.format("%s: It's %s and I was just entered!", getClass().getSimpleName(), new Date())));
-    }
-}
-
-@VaadinComponent
-@UIScope
-class ErrorView extends VerticalLayout implements View {
-
-    private Label message;
-
-    ErrorView() {
-        setMargin(true);
-        addComponent(message = new Label());
+        helpManager = new HelpManager(this);
+        setLocale(Locale.US);
+        setContent(root);
+        root.addStyleName("root");
+        root.setSizeFull();
+        // Unfortunate to use an actual widget here, but since CSS generated
+        // elements can't be transitioned yet, we must
+        Label bg = new Label();
+        bg.setSizeUndefined();
+        bg.addStyleName("login-bg");
+        root.addComponent(bg);
+        buildLoginView(false);
     }
 
-    @Override
-    public void enter(ViewChangeListener.ViewChangeEvent event) {
-        message.setValue(String.format("No such view: %s", event.getViewName()));
+    private void buildLoginView(boolean exit) {
+        if (exit) {
+            root.removeAllComponents();
+        }
+        helpManager.closeAll();
+        HelpOverlay w = helpManager
+                .addOverlay(
+                        "Welcome to the Muhuru-Bay Microgrid Dashboard",
+                        "<p>No username or password is required to view the dashboard. <br>" +
+                                "Just click the ‘Sign In’ button to continue.</p><p>Administrators, " +
+                                "please sign in",
+                        "login"
+                );
+        w.center();
+        addWindow(w);
+        addStyleName("login");
+        loginLayout = new VerticalLayout();
+        loginLayout.setSizeFull();
+        loginLayout.addStyleName("login-layout");
+        root.addComponent(loginLayout);
+
+        final CssLayout loginPanel = new CssLayout();
+        loginPanel.addStyleName("login-panel");
+
+        HorizontalLayout labels = new HorizontalLayout();
+        labels.setWidth("100%");
+        labels.setMargin(true);
+        labels.addStyleName("labels");
+        loginPanel.addComponent(labels);
+
+        Label welcome = new Label("Muhuru-Bay Microgrid DashBoard");
+        welcome.setSizeUndefined();
+        welcome.addStyleName("h2");
+        welcome.addStyleName("light");
+        labels.addComponent(welcome);
+        labels.setComponentAlignment(welcome, Alignment.MIDDLE_LEFT);
+
+//        Label title = new Label("Muhuru-Bay Micrgrid Dashboard");
+//        title.setSizeUndefined();
+//        title.addStyleName("h2");
+//        title.addStyleName("light");
+//        labels.addComponent(title);
+//        labels.setComponentAlignment(title, Alignment.MIDDLE_LEFT);
+
+        HorizontalLayout fields = new HorizontalLayout();
+        fields.setSpacing(true);
+        fields.setMargin(true);
+        fields.addStyleName("fields");
+
+        final TextField username = new TextField("Username");
+        username.focus();
+        fields.addComponent(username);
+
+        final PasswordField password = new PasswordField("Password");
+        fields.addComponent(password);
+
+        final Button signin = new Button("Sign In");
+        signin.addStyleName("default");
+        fields.addComponent(signin);
+        fields.setComponentAlignment(signin, Alignment.BOTTOM_LEFT);
+
+        final ShortcutListener enter = new ShortcutListener("Sign In",
+                ShortcutAction.KeyCode.ENTER, null) {
+            @Override
+            public void handleAction(Object sender, Object target) {
+                signin.click();
+            }
+        };
+
+        signin.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                if (username.getValue() != null
+                        && username.getValue().equals("")
+                        && password.getValue() != null
+                        && password.getValue().equals("")) {
+                    signin.removeShortcutListener(enter);
+                    buildMainView();
+                } else {
+                    if (loginPanel.getComponentCount() > 2) {
+                        // Remove the previous error message
+                        loginPanel.removeComponent(loginPanel.getComponent(2));
+                    }
+                    // Add new error message
+                    Label error = new Label(
+                            "Wrong username or password. <span>Hint: try empty values</span>",
+                            ContentMode.HTML);
+                    error.addStyleName("error");
+                    error.setSizeUndefined();
+                    error.addStyleName("light");
+                    // Add animation
+                    error.addStyleName("v-animate-reveal");
+                    loginPanel.addComponent(error);
+                    username.focus();
+                }
+            }
+        });
+
+        signin.addShortcutListener(enter);
+
+        loginPanel.addComponent(fields);
+
+        loginLayout.addComponent(loginPanel);
+        loginLayout.setComponentAlignment(loginPanel, Alignment.MIDDLE_CENTER);
     }
+
+    private void buildMainView() {
+
+    }
+
 }
